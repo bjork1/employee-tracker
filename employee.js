498fcc2c26c94cfbbf9e7ba0256c374f150ad956
Andrew Bjork <bjorkandrew1@gmail.com>
2020-03-14 15:59:44 -0500

    first push

---
+++ employee.js
@@ -0,0 +1,28 @@
+var mysql = require("mysql");
+
+var inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "",
+  database: "ice_creamDB"
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("connection as id:" + connection.threadId);
+  connection.end();
+});
+
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "test",
+      message: "Hey there"
+    }
+  ])
+
+  .then(answers => {});
