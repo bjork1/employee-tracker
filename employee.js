97e19446aa927ca2402cc422a572f119d2a625da
Andrew Bjork <bjorkandrew1@gmail.com>
2020-03-14 22:25:04 -0500

    another commit

--- employee.js
+++ employee.js
@@ -29,13 +29,20 @@ function afterConnection() {
 }
 */
 
-var questions = inquirer
+inquirer
   .prompt([
     {
       type: "list",
       name: "dropdown",
       message: "Please select from the options below",
-      choices: ["View all employees", "View all roles", "View all departments"]
+      choices: [
+        "View all employees",
+        "View all roles",
+        "View all departments",
+        "Add employee",
+        "Add role",
+        "Add department"
+      ]
     }
   ])
 
@@ -56,7 +63,6 @@ var questions = inquirer
           console.table(res);
         });
       }
-      questions;
     }
     if (answers.dropdown === "View all roles") {
       connection.connect(function(err) {
@@ -74,7 +80,8 @@ var questions = inquirer
           console.table(res);
         });
       }
-    } else if (answers.dropdown === "View all departments") {
+    }
+    if (answers.dropdown === "View all departments") {
       connection.connect(function(err) {
         if (err) throw err;
         //console.log("connected as id " + connection.threadId);
@@ -90,5 +97,68 @@ var questions = inquirer
           console.table(res);
         });
       }
+    }
+    if (answers.dropdown === "Add employee") {
+      inquirer
+        .prompt([
+          {
+            type: "input",
+            name: "first_name",
+            message: "What is the employees first name?"
+          },
+          {
+            type: "input",
+            name: "last_name",
+            message: "What is the employees last name?"
+          },
+          {
+            type: "list",
+            name: "role",
+            message: "What is the employees position?",
+            choices: [
+              "Market Analyst",
+              "Marketing Intern",
+              "Marketing Manager",
+              "Social Media Specialist",
+              "Operations Manager",
+              "Operations Analyst",
+              "Customer Service Rep",
+              "Accounting Manager",
+              "Junior Accountant",
+              "Product Owner",
+              "Scrum Master",
+              "Software Developer",
+              "Project Manager"
+            ]
+          }
+        ])
+        .then(answers => {
+          console.log(answers.first_name);
+          console.log(answers.last_name);
+          console.log(answers.role);
+
+          connection.connect(function(err) {
+            if (err) throw err;
+            //console.log("connected as id " + connection.threadId);
+            afterConnection();
+          });
+
+          function afterConnection() {
+            let sql =
+              "INSERT INTO employee(first_name, last_name, role_id) VALUES(" +
+              answers.first_name +
+              "," +
+              answers.last_name +
+              "," +
+              answers.role +
+              ");";
+            //WHERE (title = 'Marketing Intern' OR salary > 50000) ORDER BY salary ASC";
+            connection.query(sql, function(err, res) {
+              if (err) throw err;
+              //console.log(res);
+              console.table(res);
+            });
+          }
+        });
     }
   });
